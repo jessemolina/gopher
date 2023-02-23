1cedc33bb36378387ce4a73421104c6e12c56def
Jesse Molina <jesse@jessemolina.xyz>
2023-02-23 00:51:15 -0600

    Updated to catch for error first.

--- examples/stdlib/testing/table/dotproduct.go
+++ examples/stdlib/testing/table/dotproduct.go
@@ -9,12 +9,16 @@ type vector []float64
 // a1*b1 + a2*b2 + ... + an*bn
 func DotProduct(a vector, b vector) (float64, error) {
 	s := 0.0
-	if len(a) == len(b) {
-		for i := range a {
-			s += a[i] * b[i]
-		}
-	} else {
+
+	// Verify that vectors contain the same number of scalars.
+	if len(a) != len(b) {
 		return s, errors.New("Vector dimmensions don't match")
 	}
+
+	// Multiply corresponding components and add.
+	for i := range a {
+		s += a[i] * b[i]
+	}
+
 	return s, nil
 }
