c8cf930a95274acde1c71ecb0060a9027afd1741
Jesse Molina <jesse@jessemolina.xyz>
2023-02-23 00:32:06 -0600

    Updated error for vector mismatch.

--- examples/stdlib/testing/table/dotproduct.go
+++ examples/stdlib/testing/table/dotproduct.go
@@ -1,16 +1,20 @@
 package main
 
+import "errors"
+
 // Array of scalars.
 type vector []float64
 
 // Multiplies two vectors and returns a scalar value.
 // a1*b1 + a2*b2 + ... + an*bn
-func DotProduct(a vector, b vector) float64 {
+func DotProduct(a vector, b vector) (float64, error) {
 	s := 0.0
 	if len(a) == len(b) {
 		for i := range a {
 			s += a[i] * b[i]
 		}
+	} else {
+		return s, errors.New("Vector dimmensions don't match")
 	}
-	return s
+	return s, nil
 }
