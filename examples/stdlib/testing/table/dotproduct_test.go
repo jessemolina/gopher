c4338100ab8ff4fb650cebd39f5e6e1de04e470b
Jesse Molina <jesse@jessemolina.xyz>
2023-02-22 23:51:10 -0600

    Added additional tests to dot product.

--- examples/stdlib/testing/table/dotproduct_test.go
+++ examples/stdlib/testing/table/dotproduct_test.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-var message = "Results:\t %v\nExpected:\t %v\n"
+var message = "\nResults:\t %v\nExpected:\t %v\n"
 
 type testConfig struct {
 	params   []vector // array of vectors to cross multiply
@@ -15,15 +14,27 @@ type testConfig struct {
 func TestDotProduct(t *testing.T) {
 	tests := []testConfig{
 		{
-			params:   []vector{{-7, 2, 3}, {5, 2, 3}},
-			expected: -22,
+			params:   []vector{{1, 2, 3}, {1, 2, 3}}, // Basic
+			expected: 14,
+		},
+		{
+			params:   []vector{{8}, {9}}, // Single value scalar
+			expected: 72,
+		},
+		{
+			params:   []vector{{-7, -1, 0}, {-5, -6, -3}}, // Negative scalars
+			expected: 41,
+		},
+		{
+			params:   []vector{{1026, 1600}, {768, 1200}}, // Large scalars
+			expected: 2707968,
 		},
 	}
 
 	for _, test := range tests {
 		results := DotProduct(test.params[0], test.params[1])
 		if results != test.expected {
-			fmt.Printf(message, results, test.expected)
+			t.Errorf(message, results, test.expected)
 		}
 	}
 }
