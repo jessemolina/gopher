4fb0f1f45ba12837de627a82abd26eef00a10d5f
Jesse Molina <jesse@jessemolina.xyz>
2023-04-16 22:25:06 -0500

    Updated handlers to include APIMux

--- cmd/services/api/handlers/handlers.go
+++ cmd/services/api/handlers/handlers.go
@@ -3,14 +3,26 @@ package handlers
 import (
 	"net/http"
 	"net/http/pprof"
+
+	"github.com/jessemolina/gopher/cmd/services/api/handlers/v1"
 )
 
-// TODO create a debug pprof handlers
-func DebugMux() http.Handler{
+// create a debug pprof handlers
+func DebugMux() http.Handler {
 	mux := http.NewServeMux()
 
 	// register standard debug endpoints
 	mux.HandleFunc("/debug/pprof", pprof.Index)
 
-   return mux
+	return mux
+}
+
+// TODO create an api handlers
+func APIMux() http.Handler {
+	mux := http.NewServeMux()
+
+	// TODO api crud handlers
+	v1.Routes(mux)
+
+	return mux
 }
