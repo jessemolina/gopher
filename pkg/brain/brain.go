2d2182713a6f50b101b66937d1df660896c02142
Jesse Molina <jesse@jessemolina.xyz>
2023-07-27 14:42:01 -0500

    Added err check for layer func; new global todos.

--- pkg/brain/brain.go
+++ pkg/brain/brain.go
@@ -6,6 +6,11 @@ import (
 	"github.com/jessemolina/gopher/pkg/brain/math"
 )
 
+/*
+   TODO Standardize return error messages and incorporate them in table tests.
+   TODO Consider creating global options type for passing a seed to random builder functions.
+*/
+
 // Neuron is a single perceptron node.
 type Neuron struct {
 	weights []float64
@@ -46,13 +51,17 @@ func (l *Layer) WeightedSum(inputs []float64) ([]float64, error) {
 }
 
 // DenseLayer creates a new dense layer with random neuron weights and biases.
-func DenseLayer(inputs, neurons int) *Layer {
+func DenseLayer(inputs, neurons int) (*Layer, error) {
+	if inputs == 0 || neurons == 0 {
+		return nil, errors.New("Error: Invalid number of inputs or neurons.")
+	}
+
 	ns := []Neuron{}
 	for i := 0; i < neurons ; i++{
 		n := Neuron{math.RandomSlice64(inputs), 0}
 		ns = append(ns, n)
 	}
 
-	return &Layer{ns}
+	return &Layer{ns}, nil
 }
 
