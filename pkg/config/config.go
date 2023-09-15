00aabc1e6e40a8bb63d84ba06a26c0d773da3948
Jesse Molina <jesse@jessemolina.xyz>
2023-09-15 16:05:31 -0500

    Refactored config to make use of fieldInfo struct.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,36 +3,85 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 	"reflect"
 	"regexp"
 	"strings"
 )
 
-func Parse(cfg interface{}) {
-	setFlags(cfg)
+// fieldInfo represents information about a field in a struct.
+type fieldInfo struct {
+	name  string
+	desc  string
+	env   string
+	kind  reflect.Kind
+	value reflect.Value
 }
 
-func setFlags(cfg interface{}) {
-	cfgType := reflect.TypeOf(cfg).Elem()
-	for i := 0; i < cfgType.NumField(); i++ {
-		field := cfgType.Field(i)
+// Parse unmarshalls the given cfg from os env vars, flag values, and config field tags.
+func Parse(cfg interface{}) {
 
-		split := splitCamelCase(field.Name, " ")
-		snake := screamingSnakeCase(split, " ")
+	fieldsInfo := makeInfo(cfg)
 
-		name := strings.ToLower(field.Name)
-		usage := fmt.Sprintf("--%v/$%v", name, snake)
-
-		switch field.Type.Kind() {
-		case reflect.Int:
-			flag.Int(name, 0, usage)
-		case reflect.String:
-			flag.String(name, "zero", usage)
-		}
+	for _, fi := range fieldsInfo {
+		setEnv(fi)
+		setFlags(fi)
 	}
 
 	flag.Parse()
+}
 
+// makeInfo returns an array of fieldInfo for each field in cfg.
+func makeInfo(cfg interface{}) []fieldInfo {
+	fieldsInfo := []fieldInfo{}
+
+	t := reflect.TypeOf(cfg).Elem()
+	v := reflect.ValueOf(cfg).Elem()
+
+	for i := 0; i < t.NumField(); i++ {
+		name := strings.ToLower(t.Field(i).Name)
+		desc := splitCamelCase(t.Field(i).Name, " ")
+		env := screamingSnakeCase(desc, " ")
+		kind := t.Field(i).Type.Kind()
+		value := v.Field(i)
+
+		info := fieldInfo{
+			name:  name,
+			desc:  desc,
+			env:   env,
+			kind:  kind,
+			value: value,
+		}
+
+		fieldsInfo = append(fieldsInfo, info)
+	}
+
+	return fieldsInfo
+}
+
+// setEnv checks for a field's environment variable and sets if it exists.
+func setEnv(fi fieldInfo) {
+	osEnv := os.Getenv(fi.env)
+
+	// TODO Determine how to convernt osEnv strings to non-string values (i.e. int)
+	if osEnv != "" {
+		fi.value.SetString(osEnv)
+	}
+
+}
+
+// setFlag defines a cli flag for each fieldInfo.
+func setFlags(fi fieldInfo) {
+
+	msg := "%v\n--%v/$%v"
+	usage := fmt.Sprintf(msg, fi.desc, fi.name, fi.env)
+
+	switch fi.kind {
+	case reflect.String:
+		flag.String(fi.name, "", usage)
+	case reflect.Int:
+		flag.Int(fi.name, 0, usage)
+	}
 }
 
 func parseDefault(tag reflect.StructTag) string {
