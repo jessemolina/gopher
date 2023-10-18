1eaec155bf62191ccf35358a5f4c7e982364d8ef
Jesse Molina <jesse@jessemolina.xyz>
2023-10-18 10:03:18 -0500

    New App handler approach with init GET handler.

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,20 +1,49 @@
 package web
 
-import "net/http"
-
-// TODO Define web Handler function signature; http request with context.
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
 
 // TODO Build Mux struct that embeds a mux, shutdown sig, and mw functions.
 
-// TODO Factory Function for creating NewMux
+// Handler is a handle function signature that accounts for context and error.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
+// App is a custom web application.
 type App struct {
 	*http.ServeMux
 }
 
+// NewApp is creates a new custom web application.
 func NewApp() *App {
 	app := &App{
 		http.NewServeMux(),
 	}
+
 	return app
+}
+
+func (a *App) handle(method string, path string, handler Handler) {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != method {
+			http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		if err := handler(r.Context(), w, r); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}
+
+	a.HandleFunc(path, h)
+
+}
+
+// GET registers a GET method request to the provided path for the provided custom Handler.
+func (a *App) GET(path string, handler Handler) {
+	a.handle(http.MethodGet, path, handler)
 }
