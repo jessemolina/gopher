957d580af3ed2c616871bc64c40a14af6c0a5b01
Jesse Molina <jesse@jessemolina.xyz>
2024-02-17 15:44:19 -0600

    Init funcs for generating access keys.

---
+++ pkg/access/keys.go
@@ -0,0 +1,58 @@
+package access
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+)
+
+// GeneratePrivateKey creates a random 2048-bit RSA Key.
+func GeneratePrivateKey() (*rsa.PrivateKey, error) {
+	pk, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, fmt.Errorf("error generating key: %v", err)
+	}
+	return pk, nil
+}
+
+// EncodePrivatePEM encodes a Private Key PEM block into bytes.
+func EncodePrivatePEM(pk *rsa.PrivateKey) ([]byte, error) {
+	b := x509.MarshalPKCS1PrivateKey(pk)
+
+	pem, err := encodePEM(b, "PRIVATE KEY")
+	if err != nil {
+		return nil, err
+	}
+
+	return pem, nil
+}
+
+// EncodePrivatePEM encodes a Public Key PEM block into bytes.
+func EncodePublicPEM(pk *rsa.PrivateKey) ([]byte, error) {
+	b, _ := x509.MarshalPKIXPublicKey(&pk.PublicKey)
+
+	pem, err := encodePEM(b, "PUBLIC KEY")
+	if err != nil {
+		return nil, err
+	}
+
+	return pem, nil
+}
+
+// encodePEM encodes a PEM block into bytes.
+func encodePEM(b []byte, t string) ([]byte, error) {
+	buf := new(bytes.Buffer)
+	block := pem.Block{
+		Type:  t,
+		Bytes: b,
+	}
+
+	if err := pem.Encode(buf, &block); err != nil {
+		return nil, fmt.Errorf("error formatting pem block: %v", err)
+	}
+
+	return buf.Bytes(), nil
+}
