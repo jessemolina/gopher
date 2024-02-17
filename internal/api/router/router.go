ee4ea45a419ed50e4bc1e2543bb4d56f715998fe
Jesse Molina <jesse@jessemolina.xyz>
2024-02-16 22:00:54 -0600

    Init api router definition.

---
+++ internal/api/router/router.go
@@ -0,0 +1,36 @@
+package router
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/jessemolina/gopher/pkg/web"
+)
+
+// Router is a standardize mux for business api.
+type Router struct {
+	*web.Mux
+	mw []web.Middleware
+}
+
+// Config defines components required for registering Route Groups.
+type Config struct {
+	Build    string
+	Shutdown chan os.Signal
+	Log      *slog.Logger
+}
+
+// RouteGroup is defined by instances that can register routes to a web Mux based on Config.
+type RouteGroup interface {
+	Register(m *web.Mux, cfg Config)
+}
+
+// Build composes an http Handler from the given route group based on Config.
+func Build(rg RouteGroup, cfg Config) http.Handler {
+	mux := web.NewMux()
+
+	rg.Register(mux, cfg)
+
+	return mux
+}
