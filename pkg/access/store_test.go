2c06ddb98d763ff32e618459469939726495ac78
Jesse Molina <jesse@jessemolina.xyz>
2024-02-18 12:26:08 -0600

    New dummy test for writing to file.

---
+++ pkg/access/store_test.go
@@ -0,0 +1,23 @@
+package access
+
+import (
+	"testing"
+	"time"
+)
+
+
+// TODO Fix test for Writing keys/tokens to file.
+func TestWriteToFile(t *testing.T) {
+	pk, _ := GeneratePrivateKey()
+
+	pvt, _ := EncodePrivatePEM(pk)
+	pub, _ := EncodePublicPEM(pk)
+
+	rc := MakeClaimSet("Tester", "Gopher Access", time.Hour)
+	roles := []string{"ADMIN"}
+	tk, _ := GenerateToken(pk,rc, roles)
+
+	WriteToFile(pvt, "private.pem")
+	WriteToFile(pub, "public.pem")
+	WriteToFile(tk, "token")
+}
