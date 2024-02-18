991686bef2411a1a7521558f302f04b7f382e30f
Jesse Molina <jesse@jessemolina.xyz>
2024-02-18 12:25:37 -0600

    Rewrote store funcs to be more generic.

--- pkg/access/store.go
+++ pkg/access/store.go
@@ -4,15 +4,30 @@ import (
 	"os"
 )
 
-
 // TODO Build logic for reading pvt keys from file and vault.
 
+func ReadKeyFromFile() {}
+
+func ReadKeyFromVault() {}
+
 // KeyTofile writes a Key as bytes to the given path.
-func KeyToFile(key []byte, path string) error {
-	os.WriteFile(path, key, 0644)
+func WriteToFile(data []byte, path string) error {
+	err := os.WriteFile(path, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func WriteToStdout(data []byte) error {
+	_, err := os.Stdout.Write(data)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func KeyToVault() {
-}
+func WriteToVault() {}
+
+func WriteTokenToFile(token, path string) {}
