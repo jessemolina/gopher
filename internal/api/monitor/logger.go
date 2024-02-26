99f8b408c46a6f774b9528f98f15b6f28123bc42
Jesse Molina <jesse@jessemolina.xyz>
2024-02-26 00:04:05 -0600

    Split monitor mw funcs.

--- internal/api/monitor/logger.go
+++ internal/api/monitor/logger.go
@@ -1,4 +1,4 @@
-package mid
+package monitor
 
 import (
 	"context"
--- internal/api/monitor/meter.go
+++ internal/api/monitor/meter.go
@@ -1,10 +1,9 @@
-package mid
+package monitor
 
 import (
 	"context"
 	"net/http"
 
-	"github.com/jessemolina/gopher/internal/api/metrics"
 	"github.com/jessemolina/gopher/pkg/web"
 )
 
@@ -14,7 +13,7 @@ func Meter() web.Middleware {
 	mw := func(handler web.Handler) web.Handler {
 
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-			am := metrics.ApiMetrics()
+			am := ApiMetrics()
 			am.AddRequests(ctx, 1)
 
 			err := handler(ctx, w, r)
